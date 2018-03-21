b442c4328b4c648db20275ae34c59f89f6707d77
convell <paul@nevada.unr.edu>
2018-03-20 23:15:14 -0700

    Working update and select functions

--- proj2/myTest.sql
+++ proj2/myTest.sql
@@ -8,10 +8,23 @@ INSERT INTO auto values(3, 'SingleTouch', 149.99);
 INSERT INTO auto values(4, 'MultiTouch', 199.99);
 INSERT INTO auto values(5, 'SuperGizmo', 49.99);
 --;
-SELECT * FROM auto;
+SELECT * FROM auto
+WHERE price > 150;
+--;
+SELECT pid, price FROM auto
+WHERE price > 150;
+--; 
+UPDATE auto
+SET name = 'Gizmo'
+WHERE name = 'SuperGizmo';
+--;
+SELECT pid, name FROM auto;
 --;
 DELETE FROM auto 
 WHERE name = "Gizmo";
+--;
+SELECT * FROM auto
+WHERE price > 150;
 --;
 DELETE FROM auto 
 WHERE price > 150;
